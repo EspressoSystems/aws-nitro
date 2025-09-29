66089760301f766f46766532af71ba8f01c72bc9
Luke Iannucci <luke.iannucci@outlook.com>
2025-09-29 14:51:29 -0400

    changes

--- docker/server.sh
+++ docker/server.sh
@@ -8,7 +8,7 @@ while read -r message; do
     elif [ "$message" = "TERMINATE_SOCAT" ]; then
         echo "Received TERMINATE_SOCAT signal"
         pkill -KILL -f "socat.*TCP-LISTEN:2049" || echo "Failed to kill socat"
-        socat -d -d TCP-LISTEN:2049,bind=127.0.0.1,fork,reuseaddr,keepalive,ignoreeof,so-keepalive VSOCK-CONNECT:3:8004,keepalive,so-keepalive,connect-timeout=10,retry=5,interval=10 &> /tmp/socat.log &
+        socat -d -d TCP-LISTEN:2049,bind=127.0.0.1,fork,reuseaddr,so-keepalive,keepidle=60,keepintvl=30,keepcnt=5 VSOCK-CONNECT:3:8004,so-keepalive,connect-timeout=10,retry=5,interval=10 &> /tmp/socat.log &
     elif [ "$message" = "STATS" ]; then
         echo "=== STATS ==="
         echo "=== ENCLAVE MEM ==="
