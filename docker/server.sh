35cec0cf6a9fa7e888731e2bfbeb1cc73a780a66
Luke Iannucci <luke.iannucci@outlook.com>
2025-09-30 10:36:26 -0400

    try

--- docker/server.sh
+++ docker/server.sh
@@ -8,7 +8,7 @@ while read -r message; do
     elif [ "$message" = "TERMINATE_SOCAT" ]; then
         echo "Received TERMINATE_SOCAT signal"
         pkill -KILL -f "socat.*TCP-LISTEN:2049" || echo "Failed to kill socat"
-        socat -d -d -d -d -b131072 -T30 TCP-LISTEN:2049,bind=127.0.0.1,fork,reuseaddr,so-keepalive,keepidle=60,keepintvl=30,keepcnt=5,rcvbuf=1524288,sndbuf=1524288,max-children=5 VSOCK-CONNECT:3:8004,so-keepalive,connect-timeout=10,retry=5,interval=5 &> /tmp/socat.log &
+        socat -d -d -d -d -b131072 TCP-LISTEN:2049,bind=127.0.0.1,fork,reuseaddr,so-keepalive,keepidle=30,keepintvl=15,keepcnt=5,rcvbuf=1048576,sndbuf=1048576 VSOCK-CONNECT:3:8004,so-keepalive,connect-timeout=10,retry=5,interval=5 &> /tmp/socat.log &
     elif [ "$message" = "STATS" ]; then
         echo "=== STATS ==="
         echo "=== ENCLAVE MEM ==="
