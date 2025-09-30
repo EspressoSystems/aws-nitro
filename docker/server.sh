a06b1493a0ac370d9304dd77e25fbd468056e206
Luke Iannucci <luke.iannucci@outlook.com>
2025-09-30 09:33:57 -0400

    changes

--- docker/server.sh
+++ docker/server.sh
@@ -8,7 +8,7 @@ while read -r message; do
     elif [ "$message" = "TERMINATE_SOCAT" ]; then
         echo "Received TERMINATE_SOCAT signal"
         pkill -KILL -f "socat.*TCP-LISTEN:2049" || echo "Failed to kill socat"
-        socat -d -d -d -d -b131072 -T30 TCP-LISTEN:2049,bind=127.0.0.1,fork,reuseaddr,so-keepalive,keepidle=60,keepintvl=30,keepcnt=5,rcvbuf=524288,sndbuf=524288,max-children=5 VSOCK-CONNECT:3:8004,so-keepalive,connect-timeout=10,retry=5,interval=5 &> /tmp/socat.log &
+        socat -d -d -d -d -b131072 -T30 TCP-LISTEN:2049,bind=127.0.0.1,fork,reuseaddr,so-keepalive,keepidle=60,keepintvl=30,keepcnt=5,rcvbuf=1524288,sndbuf=1524288,max-children=5 VSOCK-CONNECT:3:8004,so-keepalive,connect-timeout=10,retry=5,interval=5 &> /tmp/socat.log &
     elif [ "$message" = "STATS" ]; then
         echo "=== STATS ==="
         echo "=== ENCLAVE MEM ==="
@@ -30,8 +30,12 @@ while read -r message; do
             echo "=== NITRO CPU ==="
             ps -p "$nitro_pid" -o %cpu --no-headers | awk '{printf "CPU Used: %.2f%%\n", $1}'
         fi
+        echo "=== NETSTAT ==="
+        netstat -tunap | grep -i 2049
+        echo "=== VSOCK ==="
+        ss -A vsock
     elif [ "$message" = "LOG" ]; then
-        tail -n 100 /tmp/socat.log
+        tail -n 150 /tmp/socat.log
     else
         echo "Ignoring message: $message"
     fi
