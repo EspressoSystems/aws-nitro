ea7273582086423b7c9e83174fc5d1e8f8700ab9
Luke Iannucci <luke.iannucci@outlook.com>
2025-10-01 15:41:51 -0400

    remove keepalive

--- docker/aws-nitro-entrypoint.sh
+++ docker/aws-nitro-entrypoint.sh
@@ -10,7 +10,7 @@ ENCLAVE_CONFIG_TARGET_DIR=/config # directory to copy config contents to inside 
 PARENT_SOURCE_DB_DIR=/opt/nitro/arbitrum # database path on parent directory
 
 echo "Start vsock proxy"
-socat -d -d -d -d -b24576 -T15 TCP-LISTEN:2049,bind=127.0.0.1,reuseaddr,rcvbuf=6144,sndbuf=6144 VSOCK-CONNECT:3:8004,keepalive,rcvbuf=6144,sndbuf=6144 &> /tmp/socat.log &
+socat -d -d -d -d -b24576 -T15 TCP-LISTEN:2049,bind=127.0.0.1,reuseaddr,rcvbuf=6144,sndbuf=6144 VSOCK-CONNECT:3:8004,rcvbuf=6144,sndbuf=6144 &> /tmp/socat.log &
 sleep 3
 
 echo "Mount config from ${PARENT_SOURCE_CONFIG_DIR} to ${ENCLAVE_CONFIG_SOURCE_DIR}"
