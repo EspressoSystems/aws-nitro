2354a8b63c6046123aac100138448646a5a0f785
Luke Iannucci <luke.iannucci@outlook.com>
2025-10-01 20:57:34 -0400

    again

--- docker/aws-nitro-entrypoint.sh
+++ docker/aws-nitro-entrypoint.sh
@@ -13,10 +13,9 @@ echo "Start vsock proxy"
 sudo bash -c 'cat << EOF > /tmp/socat.sh
 #!/bin/bash
 set -e
-LOGFILE=/tmp/socat-vsock.log
 while true; do
-  echo "Starting socat at \$(date)" >> \$LOGFILE
-  /usr/bin//usr/bin/socat -d -d -d -d -T5 TCP-LISTEN:2049,bind=127.0.0.1,reuseaddr,rcvbuf=65536,sndbuf=65536 VSOCK-CONNECT:3:8004,rcvbuf=65536,sndbuf=65536 &> /tmp/socat.log &
+  echo "Starting socat at \$(date)" >> /tmp/socat.log
+  /usr/bin/socat -d -d -d -d -T5 TCP-LISTEN:2049,bind=127.0.0.1,reuseaddr,rcvbuf=65536,sndbuf=65536 VSOCK-CONNECT:3:8004,rcvbuf=65536,sndbuf=65536 &> /tmp/socat.log &
   echo "socat exited with \$? at \$(date), restarting in 10 seconds" >> /tmp/socat.log
   sleep 10
 done
