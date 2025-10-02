91a88a6b9bc726857f9e6759d77f6ac10ea816a4
Luke Iannucci <luke.iannucci@outlook.com>
2025-10-01 20:59:30 -0400

    fix

--- docker/aws-nitro-entrypoint.sh
+++ docker/aws-nitro-entrypoint.sh
@@ -15,7 +15,7 @@ sudo bash -c 'cat << EOF > /tmp/socat.sh
 set -e
 while true; do
   echo "Starting socat at \$(date)" >> /tmp/socat.log
-  /usr/bin/socat -d -d -d -d -T5 TCP-LISTEN:2049,bind=127.0.0.1,reuseaddr,rcvbuf=65536,sndbuf=65536 VSOCK-CONNECT:3:8004,rcvbuf=65536,sndbuf=65536 &> /tmp/socat.log &
+  /usr/bin/socat -d -d -d -d -T5 TCP-LISTEN:2049,bind=127.0.0.1,reuseaddr,rcvbuf=65536,sndbuf=65536 VSOCK-CONNECT:3:8004,rcvbuf=65536,sndbuf=65536 &> /tmp/socat.log
   echo "socat exited with \$? at \$(date), restarting in 10 seconds" >> /tmp/socat.log
   sleep 10
 done
