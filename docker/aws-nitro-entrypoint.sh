96d0fe0291ba670a43694e38cc5a4ee721ccb500
Luke Iannucci <luke.iannucci@outlook.com>
2025-10-02 20:12:45 -0400

    reduce

--- docker/aws-nitro-entrypoint.sh
+++ docker/aws-nitro-entrypoint.sh
@@ -9,8 +9,15 @@ PARENT_SOURCE_CONFIG_DIR=/opt/nitro/config # config path on parent directory
 ENCLAVE_CONFIG_TARGET_DIR=/config # directory to copy config contents to inside enclave
 PARENT_SOURCE_DB_DIR=/opt/nitro/arbitrum # database path on parent directory
 
+echo "Set memory"
+echo 'net.ipv4.tcp_rmem = 4096 87380 16777216' >> /etc/sysctl.conf
+echo 'net.ipv4.tcp_wmem = 4096 87380 16777216' >> /etc/sysctl.conf
+echo 'net.core.rmem_max = 16777216' >> /etc/sysctl.conf
+echo 'net.core.wmem_max = 16777216' >> /etc/sysctl.conf
+sysctl -p
+
 echo "Start vsock proxy1"
-socat -b16384 TCP-LISTEN:2049,bind=127.0.0.1,fork,reuseaddr,keepalive VSOCK-CONNECT:3:8004,keepalive >/dev/null 2>&1 &
+socat TCP-LISTEN:2049,bind=127.0.0.1,fork,reuseaddr,keepalive VSOCK-CONNECT:3:8004,keepalive >/dev/null 2>&1 &
 sleep 2
 
 echo "Mount config from ${PARENT_SOURCE_CONFIG_DIR} to ${ENCLAVE_CONFIG_SOURCE_DIR}"
@@ -63,7 +70,7 @@ socat VSOCK-LISTEN:8005,fork,keepalive SYSTEM:./server.sh &
 sleep 5
 
 echo "Mount NFS database from ${PARENT_SOURCE_DB_DIR}"
-mount -t nfs4 -o rsize=32786,wsize=32786 "127.0.0.1:${PARENT_SOURCE_DB_DIR}" "/home/user/.arbitrum"
+mount -t nfs4 -o rsize=16384,wsize=16384 "127.0.0.1:${PARENT_SOURCE_DB_DIR}" "/home/user/.arbitrum"
 
 echo "Checking Mounts:"
 mount -t nfs4
