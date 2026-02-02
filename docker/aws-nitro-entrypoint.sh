8b756613ce90c368b77eff08269966d2ae5982a4
Luke Iannucci <luke.iannucci@outlook.com>
2026-02-02 16:46:42 -0500

    fixes

--- docker/aws-nitro-entrypoint.sh
+++ docker/aws-nitro-entrypoint.sh
@@ -78,11 +78,10 @@ exec /usr/local/bin/nitro \
   --validation.wasm.enable-wasmroots-check=false \
   --conf.file "${ENCLAVE_CONFIG_TARGET_DIR}/poster_config.json" \
   --node.batch-poster.parent-chain-wallet.private-key="${PRIVATE_KEY}" \
-  --espresso.batch-poster.hotshot-first-posting-block="${HOTSHOT_FIRST_POSTING_BLOCK}" \
+  --node.parent-chain.connection.url="${RPC_URL}" \
   --espresso.batch-poster.txns-monitoring-interval="${TXN_MONITOR_INTERVAL}" \
-  --espresso.batch-poster.txns-sending-interval="${TXN_SENDING_INTERVAL}" \
+  --espresso.batch-poster.txns-resubmission-interval="${TXN_RESUBMIT_INTERVAL}" \
   --espresso.streamer.hotshot-block="${HOTSHOT_BLOCK}" \
-  --espresso.streamer.address-monitor-start-l1="${ADDR_MONITOR_START}" \
   --espresso.streamer.address-monitor-step="${ADDR_MONITOR_STEP}" \
   --espresso.streamer.txns-polling-interval="${POLLING_INTERVAL}" \
   | while IFS= read -r line; do [ ${#line} -gt 4096 ] && echo "${line:0:4076}... [line truncated]" || echo "$line"; done
